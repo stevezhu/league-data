f460244a79caf2617b8077efa2e00e45c3ffbbc6
Steve Zhu <st.zhu1@gmail.com>
2016-05-01 13:42:57 -0500

    Move loading summoner ids to run.js

--- src/run.js
+++ src/run.js
@@ -12,12 +12,17 @@ suspend.run(function*() {
   var db = yield MongoClient.connect(DB_URL);
   console.log("Connected correctly to server");
 
-  var Matches = db.collection('matches'),
-      Summoners = db.collection('summoners');
+  var Matches = db.collection('matches');
+  var Summoners = db.collection('summoners');
 
-  // Load seed data if the matches collection is empty
+  // load seed data if the matches collection is empty
   if (_.isNull(yield Matches.findOne())) {
     yield* loadSeedData(db);
+  }
+
+  // load summoners if there is at least one match that hasn't been processed yet
+  if (!_.isNull(yield Matches.findOne({ processed: { $not: { $eq: true } } }))) {
+    yield* loadSummoners(db);
   }
 
   while (true) {
@@ -57,4 +62,46 @@ function* loadSeedData(db) {
   }
 
   console.log("Done loading seed data");
+};
+
+// does not connect to riot api
+// loads summoners from matches that haven't been marked as processed
+function* loadSummoners(db) {
+  var Matches = db.collection('matches');
+  var Summoners = db.collection('summoners');
+
+  console.log("Getting summoners from seed data");
+
+  // https://docs.mongodb.org/manual/reference/operator/query/not/
+  // will find documents where either done doesn't exist as a key or done != true
+  let cursor = Matches.find({ processed: { $not: { $eq: true } } });
+  while (yield cursor.hasNext()) {
+    // https://developer.riotgames.com/api/methods
+    // reference match endpoint
+    // each document is a MatchDetail object
+    let doc = yield cursor.next();
+
+    let summoners = [];
+    for (let participant of doc.participantIdentities) {
+      let player = participant.player;
+      summoners.push({
+        id: player.summonerId,
+        name: player.summonerName
+      });
+    }
+
+    for (let summoner of summoners) {
+      // use $setOnInsert and upsert: true,
+      // so that the summoner will only be inserted if it doesn't already exist in the db
+      var r = yield Summoners.updateOne(summoner, { $setOnInsert: summoner }, { upsert: true });
+      assert.equal(1, r.matchedCount + r.modifiedCount); // either modified or matched so sum must be 0
+    }
+
+    // mark that all the summoners ids have been retrieved from this match
+    var r = yield Matches.updateOne(doc, {$set: { processed: true }});
+    assert.equal(1, r.matchedCount);
+    assert.equal(1, r.modifiedCount);
+  }
+
+  console.log("Done gettings summoners from seed data");
 };
