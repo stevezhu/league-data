bb307a2c66b1d8ca0138079137107a90a5b7cb9b
Steve Zhu <st.zhu1@gmail.com>
2016-05-02 16:54:06 -0500

    Implement base riot api interface

---
+++ src/riotapi.js
@@ -0,0 +1,33 @@
+const _ = require('lodash');
+const assert = require('assert');
+const request = require('request');
+
+const BASE_URL = 'https://na.api.pvp.net/';
+class RiotApi {
+  constructor(options) {
+    _.defaults(options, {
+      region: 'na'
+    });
+    this.key = options.key;
+    this.region = options.region;
+  }
+
+  // (path, callback) or (path, params, callback)
+  callEndpoint(path, params, callback) {
+    assert(arguments.length === 2 || arguments.length === 3);
+    if (arguments.length === 2) {
+      params = {};
+      callback = arguments[1];
+    }
+
+    params.key = this.key;
+    request.get({
+      url: BASE_URL + path,
+      qs: params
+    }, function(err, response, body) {
+      callback.apply(null, arguments);
+    });
+  }
+}
+
+module.exports = RiotApi;
